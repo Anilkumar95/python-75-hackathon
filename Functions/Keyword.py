8f84929a63411114c49a0c00396295639200e4b8
yuvanbajjurla <yuvan.bajjurla@gmail.com>
2018-12-14 19:21:45 +0530

    Keywords

---
+++ Functions/Keyword.py
@@ -0,0 +1,8 @@
+#To demonstrate Key word Arguments
+def family(husband,wife):
+     print(husband,wife)
+
+
+# Keyword arguments  demonstration
+family(husband="Ravi", wife="Ramya")
+family(husband="Ramesh", wife="Natasha")  
