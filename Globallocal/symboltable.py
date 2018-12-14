952b29f140650a7bee31f437031fa626e099a3b4
yuvanbajjurla <yuvan.bajjurla@gmail.com>
2018-12-14 21:41:40 +0530

    symboltable

---
+++ Globallocal/symboltable.py
@@ -0,0 +1,10 @@
+#This function returns the symbol table as a dictionary
+symbol = locals()
+print(symbol)
+
+#to perform local subtraction
+def sam(x,y):
+    sam = x-y
+    return sam
+
+print(sam(88,82))
