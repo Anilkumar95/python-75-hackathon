9075f55290acc2aca76d11dba3ab4ae034862f4d
yuvanbajjurla <yuvan.bajjurla@gmail.com>
2018-12-14 18:24:14 +0530

    checking in a tuple

---
+++ Tuples/check.py
@@ -0,0 +1,11 @@
+#Checking if an item exists in a tuple or not
+animals=("Lion","Zebra","Elephant","leopard")
+if "Lion" in animals:
+    print("true")
+
+#Other conditions
+animals=("Lion","Zebra","Elephant","leopard")
+if "Frog" in animals:
+    print("false")
+else:
+    print("Not there")
