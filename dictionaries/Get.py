b745298875e137a20041698f7e36f845e9447833
yuvanbajjurla <yuvan.bajjurla@gmail.com>
2018-12-14 14:36:13 +0530

    Accessing Data

---
+++ dictionaries/Get.py
@@ -0,0 +1,10 @@
+#Accessing specific elemnts from a dictionary
+Breakfast={
+"Name":"dosa",
+"cost": 45,
+"Proteins": 4,
+"Fat":2
+}
+#Finding the cost of Breakfast
+p=Breakfast.get("cost")
+print(p)
