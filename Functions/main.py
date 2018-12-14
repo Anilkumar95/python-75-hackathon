f0bf18d74b7beb083a941bfc0ae4ad08518dc227
yuvanbajjurla <yuvan.bajjurla@gmail.com>
2018-12-14 19:54:03 +0530

    Square Root

--- Functions/main.py
+++ Functions/main.py
@@ -1,6 +1,7 @@
+#simple function
 def Welcome():
     print("Welcome to 2019")
-#Main function
+#Main function defintion
 def main():
     print("Main would be printed first")
     Welcome()
---
+++ Functions/math.py
@@ -0,0 +1,8 @@
+import math
+#square root of a number
+def square(one):
+
+    print(math.sqrt(one))
+    return
+
+square(25)
