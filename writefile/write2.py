11e2d14a8565eea818a661e91b0901158dc4576f
yuvanbajjurla <yuvan.bajjurla@gmail.com>
2018-12-15 09:44:30 +0530

    Writing

--- writefile/write2.py
+++ writefile/write2.py
@@ -1,4 +1,4 @@
 #Displaying the mode encoding and name of the file
 write = open("file.txt", "w")
-write.write("Woops! I have deleted the content!")
+write.write("{{{{{{{{{{{{55555+677}}}}}}}}}}}}")
 print(write)
