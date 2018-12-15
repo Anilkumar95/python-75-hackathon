c7d8b5a8bb788706b9e98a853f3e1398bc945a20
yuvanbajjurla <yuvan.bajjurla@gmail.com>
2018-12-15 17:43:40 +0530

    Lambda

---
+++ Lambda/l1.py
@@ -0,0 +1,6 @@
+h = lambda sum : sum + 10
+print(h(80))
+
+
+h = lambda name: name * 10
+print(h("Hii"))
---
+++ Lambda/l2.py
@@ -0,0 +1,8 @@
+y = lambda a, b : a * b
+print(y(50, 60))
+
+d= lambda a, b : a / b
+print(d(8, 9))
+
+m = lambda a, b : a % b
+print(m(54, 69))
